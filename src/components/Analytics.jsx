24f4eb766acc32e2808e46eda45bdf9e2d68bf64
astkout <astkout12@hotmail.com>
2024-01-21 20:51:22 +0000

    Initial commit

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Laptop from "../assets/laptop.jpg";
+
+const Analytics = () => {
+  return (
+    <div className="w-full bg-white py-16 px-4">
+      <div className="max-w-[1224px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+        <img src={Laptop} alt="Laptop" className="w-full h-auto" />
+        <div className="text-left">
+          <p className="text-[#00df9a] text-xl font-bold">
+            DATA ANALYTICS DASHBOARD
+          </p>
+          <h1 className="text-4xl md:text-5xl font-bold mt-4">
+            Manage Data Analytics Centrally
+          </h1>
+          <p className="text-gray-500 mt-4">
+            Centralized data analytics consolidates and oversees data processes
+            from a central platform, streamlining collection, storage, and
+            analysis. This approach fosters better decision-making,
+            collaboration, and security, enabling organizations to derive
+            maximum value from their data. It promotes a data-driven culture,
+            fostering innovation and a competitive advantage.
+          </p>
+          <button className="bg-black text-[#00df9a] w-[200px] rounded font-medium my-6 mx-auto px-3 py-3">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Analytics;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 className="text-3xl font-bold text-[#00df9a]">FINANCE.</h1>
+      <ul className={`flex ${isMenuOpen ? "hidden" : "visible"} md:flex hidden`}>
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+      <div className="md:hidden" onClick={toggleMenu}>
+        {isMenuOpen ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+      {isMenuOpen && (
+        <div className="fixed left-0 top-0 w-[40%] h-screen border-r border-gray-900 bg-[#000300] transition-all ease-in-out duration-300">
+          <h1 className="text-3xl font-bold text-[#00df9a] m-4">FINANCE.</h1>
+          <ul className="pt-24 uppercase">
+            <li className="p-4 border-b">Home</li>
+            <li className="p-4 border-b">Company</li>
+            <li className="p-4 border-b">Resources</li>
+            <li className="p-4 border-b">About</li>
+            <li className="p-4 border-b">Contact</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
